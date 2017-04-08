9e8faf4b217a783b3001be5f5e38fa8595b66e97
dianacamacho <dcam1901@yahoo.com>
2017-04-08 02:43:04 -0500

    start changing book_list comp to container, import connect from react-redux

---
+++ src/containers/book_list.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map((book) => {
+      return (
+        <li className="list-group-item" key={book.title}>{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
