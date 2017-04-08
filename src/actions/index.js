a9e1681eb93b81199c411f861e5eddf9b36a717b
dianacamacho <dcam1901@yahoo.com>
2017-04-08 03:55:28 -0500

    update action creator to return action object with type and payload, call the action creator on click in BookList

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 export function selectBook(book) {
-  // return {
-  //   type: 'BOOK_SELECTED',
-  //   payload: book
-  // }
-  console.log(book.title);
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
 }
--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -7,7 +7,12 @@ class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li className="list-group-item" key={book.title}>{book.title}</li>
+        <li 
+          key={book.title}
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)}>
+          {book.title}
+        </li>
       );
     });
   }
