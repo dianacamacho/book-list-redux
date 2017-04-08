26aa33e962f0d72fe5276280846ae5c3dff01812
dianacamacho <dcam1901@yahoo.com>
2017-04-08 02:22:37 -0500

    BookReducer file to return list of books for books state property

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// reducer function returning array of books
+export default function() {
+  return [
+    { title: 'JS Good Parts', pages: 85 },
+    { title: 'Harry Potter', pages: 1085 },
+    { title: 'The Dark Tower', pages: 285 },
+    { title: 'Eloquent Ruby', pages: 35 }
+  ]
+}
